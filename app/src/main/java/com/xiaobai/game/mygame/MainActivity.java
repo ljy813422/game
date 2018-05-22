c52c7c328ee451c899e49d92be1babf6ee692dbf
ljy123 <709973880@qq.com>
2018-05-22 18:27:30 +0800

    welcome

--- app/src/main/java/com/xiaobai/game/mygame/MainActivity.java
+++ app/src/main/java/com/xiaobai/game/mygame/MainActivity.java
@@ -1,13 +1,34 @@
 package com.xiaobai.game.mygame;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.login).setOnClickListener(this);
+        findViewById(R.id.exit).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                break;
+            case R.id.exit:
+                finish();
+                break;
+        }
+
+    }
+
+    private void exit(){
+        AlertDialog dialog = new AlertDialog(this);
     }
 }

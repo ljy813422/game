fa2ac4b9c58d537aa2ee26ca98a86772c0474326
ljy123 <709973880@qq.com>
2018-05-23 10:13:44 +0800

    退出

--- app/src/main/java/com/xiaobai/game/mygame/MainActivity.java
+++ app/src/main/java/com/xiaobai/game/mygame/MainActivity.java
@@ -1,11 +1,15 @@
 package com.xiaobai.game.mygame;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +22,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.login:
                 break;
             case R.id.exit:
-                finish();
+                exit();
                 break;
         }
 
     }
 
-    private void exit(){
-        AlertDialog dialog = new AlertDialog(this);
+    private void exit() {
+        if (alertDialog == null) {
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle(getResources().getString(R.string.exit))
+                    .setMessage(getResources().getString(R.string.sure_to_exit))
+                    .setNegativeButton(getResources().getString(R.string.exit), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    }).setPositiveButton(getResources().getString(R.string.cancle), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            alertDialog = builder.create();
+        }
+        alertDialog.show();
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if(keyCode ==KeyEvent.KEYCODE_BACK){
+            exit();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }

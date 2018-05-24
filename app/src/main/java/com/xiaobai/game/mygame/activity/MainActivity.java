ab754ffdbb27974de36a56051dfa053f0205a48b
ljy123 <709973880@qq.com>
2018-05-24 16:44:06 +0800

    相机  动画

--- app/src/main/java/com/xiaobai/game/mygame/activity/MainActivity.java
+++ app/src/main/java/com/xiaobai/game/mygame/activity/MainActivity.java
@@ -1,26 +1,120 @@
-package com.xiaobai.game.mygame;
+package com.xiaobai.game.mygame.activity;
 
+import android.animation.ValueAnimator;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+
+import com.xiaobai.game.mygame.R;
 import com.xiaobai.game.mygame.test.TestActivity;
 import com.xiaobai.game.mygame.util.DialogUtil;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+<<<<<<< .mine
 
+=======
+
+>>>>>>> .theirs
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    @BindView(R.id.up_container)
+    LinearLayout upContainer;
+    @BindView(R.id.up_scoll_container)
+    ScrollView upScollContainer;
+    @BindView(R.id.down_container)
+    RelativeLayout downContainer;
+    @BindView(R.id.down_scoll_container)
+    ScrollView downScollContainer;
+    @BindView(R.id.place_holder_view)
+    View placeHolderView;
+    @BindView(R.id.up_place_holder_view)
+    View upPlaceHolderView;
+    @BindView(R.id.up_rl_container)
+    RelativeLayout upRlContainer;
+
     private AlertDialog alertDialog;
+    private DisplayMetrics dm;
+    //是否显示 login
+    private boolean isLogin = true;
+    private ValueAnimator comeAnimator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         findViewById(R.id.login).setOnClickListener(this);
         findViewById(R.id.exit).setOnClickListener(this);
+
+        dm = getResources().getDisplayMetrics();
+
+
+        Log.d("MainActivity", "" + dm.heightPixels + "-----" + dm.widthPixels);
+        upContainer.getLayoutParams().height = dm.heightPixels;
+        downContainer.getLayoutParams().height = dm.heightPixels;
+        placeHolderView.getLayoutParams().height = dm.heightPixels / 2;
+        upPlaceHolderView.getLayoutParams().height = dm.heightPixels / 2;
+        upRlContainer.getLayoutParams().height = dm.heightPixels / 2;
+        upScollContainer.scrollTo(0, dm.heightPixels / 2);
+        upScollContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        downScollContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (isLogin) {
+            comeAnimator = ValueAnimator.ofInt(0, dm.heightPixels / 2);
+            comeAnimator.setDuration(1000);
+            comeAnimator.setInterpolator(new AccelerateInterpolator());
+            comeAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+
+                    int value = (int) animation.getAnimatedValue();
+                    upScollContainer.scrollTo(0, dm.heightPixels / 2 - value);
+                    downScollContainer.scrollTo(0, value);
+                }
+            });
+            comeAnimator.start();
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (comeAnimator != null) {
+            comeAnimator.cancel();
+        }
     }
 
     @Override
@@ -72,7 +166,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-        if(keyCode ==KeyEvent.KEYCODE_BACK){
            // exit();
             DialogUtil.exit(this);
         }
